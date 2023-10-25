887acede2da96ce8daa84b6d1efef5fe763f2871
Kish29 <875691208@qq.com>
2023-10-25 17:22:05 +0800

    remove from
    Change-Id: I4765af28853586b1fc29bf0c0f0d9c10cab21cac

--- src/char_stream.rs
+++ src/char_stream.rs
@@ -9,28 +9,3 @@ pub trait CharStream: IntStream {
     /// Guaranteed to not throw an exception
     fn text(&self, start: usize, end: usize) -> Cow<'_, str>;
 }
-
-/// create a new [CharStream] from str.
-pub fn from_str(s: &str) -> Box<dyn CharStream> {
-    Box::new(StringStream::new(s.to_string()))
-}
-
-/// create a new [CharStream] from bytes.
-pub fn from_bytes(b: Vec<u8>) -> Box<dyn CharStream> {
-    Box::new(ByteStream::new(b))
-}
-
-/// create a new [CharStream] from code points 8bit.
-pub fn from_code_point8bits(b: Vec<u8>) -> Box<dyn CharStream> {
-    Box::new(CodePoint8BitStream::new(b))
-}
-
-/// create a new [CharStream] from code points 16bit.
-pub fn from_code_point16bits(b: Vec<u16>) -> Box<dyn CharStream> {
-    Box::new(CodePoint16BitStream::new(b))
-}
-
-/// create a new [CharStream] from code points 32bit.
-pub fn from_code_point32bits(b: Vec<u32>) -> Box<dyn CharStream> {
-    Box::new(CodePoint32BitStream::new(b))
-}
